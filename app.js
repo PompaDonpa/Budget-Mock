7608025ccc13c08b261f2897287ac71fd1d95487
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-15 17:04:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//Dependencies
+const express = require("express");
+const app = express();
+const cors = require("cors")
+
+
+//Middleware
+
+app.use(express.json()); 
+app.use(cors());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the budgetting app!");
+});
+
+module.exports = app
